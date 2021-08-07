f9677dbd8c5767e74e5fd3e261c2f71df7a897d0
Hugo Ramos Bello <hugorb45@gmail.com>
2021-08-07 18:12:32 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,20 +1,26 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
@@ -22,6 +28,8 @@ public class DepartmentFormController implements Initializable{
 	private Department department;
 	
 	private DepartmentService departmentService;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
@@ -49,15 +57,36 @@ public class DepartmentFormController implements Initializable{
 		try {
 			department = getFormData();
 			departmentService.saveOrUpdate(department);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		}
 		catch (DbException e) {
 			Alerts.showAlert("Erro saving department", null, e.getMessage(), AlertType.ERROR);
 		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErros());
+		}
 	}
 	
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}	
+	}
+
 	private Department getFormData() {
+		
+		ValidationException exception = new ValidationException("Validation erro");
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addErro("name", "Field can't be empty");
+		}
+		
 		Department department = new Department(Utils.tryParceToInt(txtId.getText()), txtName.getText());
+		
+		if(exception.getErros().size() > 0) {
+			throw exception;
+		}
+		
 		return department;
 	}
 
@@ -90,6 +119,17 @@ public class DepartmentFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(department.getId()));
 		txtName.setText(department.getName());
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 
 }
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,7 +27,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable{
+public class DepartmentListController implements Initializable, DataChangeListener{
 	
 	private DepartmentService departmentService;
 	
@@ -94,6 +95,7 @@ public class DepartmentListController implements Initializable{
 			DepartmentFormController controller = loader.getController();
 			controller.setDepertment(department);
 			controller.setDepartmentService(new DepartmentService());
+			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
@@ -109,6 +111,12 @@ public class DepartmentListController implements Initializable{
 			Alerts.showAlert("IOException", "Erro loading view", e.getMessage(), AlertType.ERROR);
 		}
 		
+	}
+
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
 	}
 
 }
